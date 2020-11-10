8b1267ebb475af4db4f937ce3a4ec023cc1591ef
Rachit.Sharma <rachit.sharma@ibm.com>
2020-11-10 15:44:09 -0500

    Lab 4

---
+++ lab4/cgi-bin/part1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use CGI':standard';
+use strict;
+print "Content-type: text/html \n\n";
+
+print "<HTML>";
+print "<HEAD>";
+print "<TITLE>CPS530</TITLE>";
+print qq~<link rel="stylesheet" type="text/css" href="mystyle.css">\n~;
+print "</HEAD>";
+print "<BODY>";
+print "<p>This is my first Perl program </p>";
+print "</BODY>";
+print "</HTML>";
